36341e8e3aa5bd7106498fed86eab2c6496ce6b4
ariestav <ariestav@sbcglobal.net>
2016-02-21 02:04:27 -0600

    Better CLI execution experience
    Provided there is a properly configured and readable templater-options.json file, the batch file will display the last logged messager and error if there are any.
    
    Signed-off-by: ariestav <ariestav@sbcglobal.net>

--- Windows/templater.bat
+++ Windows/templater.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
 ::Get command line parameters
 if "%~1"=="" (set APP_VER="null") else (set APP_VER=%1)
@@ -34,8 +35,82 @@ if not defined or_ (
     exit /B
 )
 
+::Verify the templater-options.json file exists in the current directory
+if not exist "%CD%\templater-options.json" (
+  echo Templater CLI Error: No cli configuration file found
+  echo.
+  echo Ensure "templater-options.json" file exists in =^>
+  echo "%CD%" 
+  echo.
+  echo See you next time :^)
+  echo.
+  exit /B
+)
+
+::Get log location from templater-options file.
+
+for /f "tokens=1,2,*" %%a in (' find ":" ^< "templater-options.json" ') do (
+  if %%a=="log_location": (
+    set log_loc=%%~b%%~c
+    call:GetLogLoc log_path "%log_loc%"
+  )
+)
+
+echo.
+echo - - - - - - - - - - - - - -
+echo.
+echo Templater 2 CLI Configuration (templater-options.json)
+echo.
+echo Configuration loaded from =^>
+echo %CD%\templater-options.json
+echo.
+echo log_location : %log_path%
+echo.
+echo.
+
 ::For safe measure, copy the installed jsxbin to the current folder
+echo Now launching Adobe After Effects
 copy /Y "%APP_DIR%\%PANELS%\Templater 2.jsxbin" "%CD%\Templater 2.jsxbin"
 
 ::Run After Effects along with the Templater 2 file
 "%APP_DIR%\afterfx" -r %USE_UI% "%CD%\Templater 2.jsxbin"
+
+echo.
+echo.
+echo Done with After Effects processing.  See log files below for more information =^>
+echo.
+dir "%log_path%"
+
+if exist "%log_path%\templater.log" (
+   for /F "delims=" %%a in (%log_path%\templater.log) do (
+     set "previous=!last!"
+     set "last=%%a"
+   )
+ echo.
+ echo.
+ echo LAST LOGGED MESSAGE =^>
+ echo.
+ echo !previous!
+ echo.
+ echo.
+)
+
+if exist "%log_path%\templater.err" (
+   for /F "delims=" %%a in (%log_path%\templater.err) do (
+     set "previous=!last!"
+     set "last=%%a"
+   )
+ echo LAST REPORTED ERROR =^>
+ echo.
+ echo !previous!
+ echo.
+ echo.
+)
+
+:GetLogLoc
+ set #=%~2
+ set log_path=%#:\\=\%
+GOTO :EOF
+
+echo.
+echo.
